e1076606cdcbc19ec5e17f2e0d132b72a6ca8998
Carmen Caniglia <carmencaniglia@gmail.com>
2023-10-01 15:22:03 +0200

    completamento sito con animazione M

--- base_di_partenza/assets/js/script.js
+++ base_di_partenza/assets/js/script.js
@@ -1,10 +1,23 @@
-const head = document.querySelector("#navBar");
-
-const changeColor = function () {
-  if (window.scroll > 100) {
-    head.style.backgroundColor = "#ffc017";
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navBar");
+  const button = document.getElementsByClassName("started")[0];
+  if (this.window.scrollY > 455) {
+    navbar.style.backgroundColor = "white";
+    button.style.backgroundColor = "#1a8917";
   } else {
-    head.style.backgroundColor = "#ffffff";
+    navbar.style.backgroundColor = "#ffc017";
+    button.style.backgroundColor = "#191919";
   }
+});
+
+const emme = () => {
+  const elements0 = document.querySelectorAll("svg > g > g > g[opacity = '0']");
+  const elements1 = document.querySelectorAll("svg >g > g > g[opacity='1']");
+
+  let rnd = Math.floor(Math.random() * elements0.length);
+  elements0[rnd].setAttribute("opacity", "1");
+
+  rnd = Math.floor(Math.random() * elements1.length);
+  elements1[rnd].setAttribute("opacity", "0");
 };
-window.addEventListener("scroll", changeColor);
+setInterval(emme, 200);
